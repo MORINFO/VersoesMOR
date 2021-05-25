5ea6797a8948a991fbf65aea1979cb08baa0afcf
Nicolas <nicolas.7l@hotmail.com>
2021-05-25 09:24:35 -0300

    ACERTO

--- start/routes.ts
+++ start/routes.ts
@@ -3,6 +3,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { Mensagem: 'Sistema de verificação de versões dos sistemas MOR' }
 })
-Route.post('/atualizaversao', 'VersoesController.store')
+Route.get('/atualizaversao', 'VersoesController.store')
 
 Route.get('/versoes', 'VersoesController.index')
