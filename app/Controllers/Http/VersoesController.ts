78b135f44aa417c583f0a9603260e8e22b55f66d
Nicolas <nicolas.7l@hotmail.com>
2021-05-26 17:26:45 -0300

    ajustes no envio de email

--- app/Controllers/Http/VersoesController.ts
+++ app/Controllers/Http/VersoesController.ts
@@ -1,52 +1,64 @@
-/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Mail from '@ioc:Adonis/Addons/Mail'
 import Versao from 'App/Models/Versoes'
+import Database from '@ioc:Adonis/Lucid/Database'
+
 
 export default class VersoesController {
-  async index({}: HttpContextContract) {
+  async index({ }: HttpContextContract) {
     const versoes = await Versao.all()
 
     return versoes
   }
 
-  async store({ request, response }: HttpContextContract) {
+  async store({ response, request }: HttpContextContract) {
+
     try {
       const dados = request.only(['empresa', 'faturamento', 'financeiro', 'compras'])
 
       let pesquisaEmpresa = await Versao.findBy('empresa', dados.empresa)
 
-      if (pesquisaEmpresa) {
-        pesquisaEmpresa.merge(dados)
-        await pesquisaEmpresa.save()
+      let consulta =
+      {
+        empresa: dados.empresa,
+        sistema: dados.faturamento && 'faturamento' || dados.financeiro && 'financeiro' || dados.compras && 'compras',
+        versao: dados.faturamento && dados.faturamento || dados.financeiro && dados.financeiro || dados.compras && dados.compras
+      }
 
+      let desatualizado = await Database
+        .from('versoes')
+        .select('empresa', `${consulta.sistema} AS versao`)
+        .where(consulta.sistema, '<>', consulta.versao)
+
+      if (desatualizado) {
         await Mail.sendLater((message) => {
           message
             .from('morinfo@morinfo.com.br')
             .to('suporte@morinfo.com.br')
             .cc('nicolas@morinfo.com.br', 'ronaldo@morinfo.com.br')
-            .subject('Aviso de Atualização Sistema MOR')
-            .htmlView('emails/email', { dados })
+            .subject('Aviso de Empresas desatualizadas')
+            .htmlView('emails/email', { desatualizado, sistema: consulta.sistema})
         })
+
+      }
+
+      if (pesquisaEmpresa) {
+        pesquisaEmpresa.merge(dados)
+
+        await pesquisaEmpresa.save()
+
+      } else {
+        await Versao.create(dados)
+
         return
       }
-      await Versao.create(dados)
-      await Mail.sendLater((message) => {
-        message
-          .from('suporte@morinfo.com.br')
-          .to('suporte@morinfo.com.br')
-          .cc('nicolas@morinfo.com.br', 'ronaldo@morinfo.com.br')
-          .subject('Aviso de Atualização Sistema MOR')
-          .htmlView('emails/email', { dados })
-      })
 
-      return
     } catch (error) {
       return response.send(error)
     }
   }
 
-  async show({}: HttpContextContract) {}
+  async show({ }: HttpContextContract) { }
 
-  async update({}: HttpContextContract) {}
+  async update({ }: HttpContextContract) { }
 }
