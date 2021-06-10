3a64d5dfa5025696f618aafa88020cf8bd3e9996
Nicolas <nicolas.7l@hotmail.com>
2021-06-10 16:10:57 -0300

    acerto na rota de bkp

--- app/Controllers/Http/BkpsController.ts
+++ app/Controllers/Http/BkpsController.ts
@@ -3,21 +3,28 @@ import Backups_clientes from 'App/Models/Backups_clientes'
 
 export default class BkpsController {
 
-  public async store ({ request }: HttpContextContract) {
+  public async store({ request, response }: HttpContextContract) {
 
-    const empresa = request.only(['empresa'])
+    const empresa = request.all()
 
-    const pesquiaEmpresa = await Backups_clientes.findBy('empresa', empresa)
+    try {
+      const pesquiaEmpresa = await Backups_clientes.findBy('empresa', empresa)
 
-    if(pesquiaEmpresa){
+      if (!pesquiaEmpresa) {
 
-      await Backups_clientes.create(empresa)
+        await Backups_clientes.create(empresa)
+        return
+      }
+
+      console.log(empresa)
+      pesquiaEmpresa.merge(empresa)
+      await pesquiaEmpresa.save()
+
+      return
+    } catch (error) {
+      return response.status(500).send(error)
     }
 
-    pesquiaEmpresa?.merge(empresa)
-    await pesquiaEmpresa?.save()
-
-    return
   }
 
 }
--- start/routes.ts
+++ start/routes.ts
@@ -3,6 +3,11 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { Mensagem: 'Sistema de verificação de versões dos sistemas MOR' }
 })
+
+// Rotas para Versoes
 Route.get('/atualizaversao', 'VersoesController.store')
 
 Route.get('/versoes', 'VersoesController.index')
+
+//Rotas para controle de Bkps
+Route.get('/bkp', 'BkpsController.store')
